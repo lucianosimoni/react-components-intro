63d57604037470f821fa231f6e9886f4e5d85760
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-11-03 12:41:00 +0000

    Components More defined
    Now the Main.js component has more divisions:
    - Logo
    - SearchBar
    - SearchButtons
    - > Button
    
    SearchButtons has another component neasted in it.
    The Button component receives properties, and we've defined
    buttonText as a custom property, which is used when Rendering the
    Button itself.

---
+++ src/Button.js
@@ -0,0 +1,7 @@
+function Button (properties) {
+    return (
+        <button>{properties.buttonText}</button>
+    )
+}
+
+export default Button
---
+++ src/SearchButtons.js
@@ -0,0 +1,12 @@
+import Button from './Button'
+
+function SearchButtons () {
+    return (
+        <div className="buttons">
+            <Button buttonText="Google Search" />
+            <Button buttonText="Feeling Lucky" />
+        </div>
+    )
+}
+
+export default SearchButtons

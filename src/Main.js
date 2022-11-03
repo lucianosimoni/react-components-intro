d97d1eb4a3328a7e233bfc0bd02479537d7fbedc
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-11-03 12:53:55 +0000

    CSS Changes
    Now All major components have a individual CSS
    Not that concize tho, but yep, good for the learning.

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,8 @@ import Logo from './Logo'
 import SearchBar from './SearchBar'
 import SearchButtons from './SearchButtons'
 
+import './main.css'
+
 function Main () {
     return (
         <main>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
-import './styles.css'
+import './app.css'
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(
